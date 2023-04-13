414b4b4290dc0392dc5a0375c000bb7ec27e8f04
pablo-Lenovo-ideapad-330-15IKB <pablolandimdesa@gmail.com>
2023-04-13 20:17:34 -0300

    Body
    Colocando cor no Body, deixando a cor branca ainda, só para brincar

--- pages/_document.js
+++ pages/_document.js
@@ -4,7 +4,7 @@ export default function Document() {
   return (
     <Html lang="en">
       <Head />
-      <body className='bg-gray-800 flex flex-col' >
+      <body className='bg-white flex flex-col' >
         <Main />
         <NextScript />
       </body>
