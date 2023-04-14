07a25a7cf80ebe3a33200e22abed64975af6cadf
pablo-Lenovo-ideapad-330-15IKB <pablolandimdesa@gmail.com>
2023-04-14 16:16:26 -0300

    Header
    deixando simétrico

--- components/Header.jsx
+++ components/Header.jsx
@@ -3,20 +3,18 @@ import { BsGithub, BsLinkedin } from 'react-icons/bs';
 
 const Header = () => {
   return (
-    <header className='bg-gray-800 flex flex-col justify-center items-center h-32 w-full'>
-      <div className="flex flex-col items-center mt-4 sm:flex-row sm:items-center sm:justify-between">
-        <div className="mb-2 sm:mb-0 sm:mr-auto">
-          <h1 className='text-blue-500 text-4xl font-bold text-center'
-            style={{
-              textShadow: '0px 0px 10px rgba(0, 191, 255, 0.5)',
-              boxShadow: '0px 0px 10px rgba(0, 191, 255, 0.5)',
-              padding: '0.3em',
-              borderRadius: '0.5em',
-            }}>
-            Pablo Landim de Sá
-          </h1>
-        </div>
-        <div className="flex justify-start sm:justify-end">
+    <header className='bg-gray-800 flex justify-center items-center h-32 w-full'>
+      <div className="flex items-center justify-between w-full max-w-4xl px-4">
+        <h1 className='text-blue-500 text-4xl font-bold text-center'
+          style={{
+            textShadow: '0px 0px 10px rgba(0, 191, 255, 0.5)',
+            boxShadow: '0px 0px 10px rgba(0, 191, 255, 0.5)',
+            padding: '0.3em',
+            borderRadius: '0.5em',
+          }}>
+          Pablo Landim de Sá
+        </h1>
+        <div className="flex items-center justify-end">
           <a href='https://www.linkedin.com/in/pablolandimdesadev/'
             target="_blank" rel="noreferrer"
             className='text-white mx-2 text-xl'>
@@ -31,6 +29,7 @@ const Header = () => {
         </div>
       </div>
     </header>
+
 
 
 
