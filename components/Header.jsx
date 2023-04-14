a5277442fc913aea225e463703a4b8812e77b069
pablo-Lenovo-ideapad-330-15IKB <pablolandimdesa@gmail.com>
2023-04-14 16:45:59 -0300

    Header
    deixando simétrico e melhor / pŕoximo passo é dark-mode

--- components/Header.jsx
+++ components/Header.jsx
@@ -3,32 +3,38 @@ import { BsGithub, BsLinkedin } from 'react-icons/bs';
 
 const Header = () => {
   return (
-    <header className='bg-gray-800 flex justify-center items-center h-32 w-full'>
-      <div className="flex items-center justify-between w-full max-w-4xl px-4">
+
+    <header className='bg-gray-800 flex flex-col justify-center items-center h-32 w-full'>
+      <div className="flex flex-col sm:flex-row items-center justify-center w-full px-4">
         <h1 className='text-blue-500 text-4xl font-bold text-center'
           style={{
             textShadow: '0px 0px 10px rgba(0, 191, 255, 0.5)',
             boxShadow: '0px 0px 10px rgba(0, 191, 255, 0.5)',
             padding: '0.3em',
             borderRadius: '0.5em',
+            whiteSpace: 'nowrap'
           }}>
           Pablo Landim de Sá
         </h1>
-        <div className="flex items-center justify-end">
-          <a href='https://www.linkedin.com/in/pablolandimdesadev/'
-            target="_blank" rel="noreferrer"
-            className='text-white mx-2 text-xl'>
-            <BsLinkedin />
-          </a>
-          <a href='https://github.com/PabloLSa'
-            target="_blank" rel="noreferrer"
-            className='text-white mx-2 text-xl'
-          >
-            <BsGithub />
-          </a>
+        <div className="flex items-center mt-2">
+          <span className='text-white mx-2 text-xl flex items-center'>
+            <a href='https://www.linkedin.com/in/pablolandimdesadev/'
+              target="_blank" rel="noreferrer">
+              <BsLinkedin />
+            </a>
+          </span>
+          <span className='text-white mx-2 text-xl flex items-center'>
+            <a href='https://github.com/PabloLSa'
+              target="_blank" rel="noreferrer">
+              <BsGithub />
+            </a>
+          </span>
         </div>
       </div>
     </header>
+
+
+
 
 
 
