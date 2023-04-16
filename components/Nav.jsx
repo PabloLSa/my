a59d3353ee8e81e1a3628da1b42c5a8fa59630ef
pablo-Lenovo-ideapad-330-15IKB <pablolandimdesa@gmail.com>
2023-04-16 16:49:59 -0300

    Nav
    Nav com dark mode ok

--- components/Nav.jsx
+++ components/Nav.jsx
@@ -1,10 +1,23 @@
 import React from 'react';
+import ThemeContext from '@/context/ThemeContext';
+import { useState, useContext, useEffect } from 'react';
 import Image from 'next/image';
 
 
 function Nav() {
+  const theme = useContext(ThemeContext);
+  // console.log(theme);
+    const [themeClasses, setThemeClasses] 
+    = useState('bg-gray-600 h-full');
+    useEffect(() => {
+      setThemeClasses(
+        theme.color === 'light'
+          ? 'bg-gray-500 h-full'
+          : 'bg-gray-600 h-full'
+      );
+    }, [theme.color]);
   return (
-    <div className='bg-gray-700 h-full'>
+    <div className={themeClasses}>
       <Image src='/myphoto.jpeg' width={300} height={200} alt='Minha Foto' />
     </div>
   );
