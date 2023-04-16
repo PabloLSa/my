7696a43f8d109ded1373dfee6aeaa7fe798bb632
pablo-Lenovo-ideapad-330-15IKB <pablolandimdesa@gmail.com>
2023-04-16 19:34:22 -0300

    Nav
    Ajustes de cores back-ground

--- components/Nav.jsx
+++ components/Nav.jsx
@@ -6,31 +6,33 @@ import Image from 'next/image';
 
 function Nav() {
   const theme = useContext(ThemeContext);
-  const [themeClasses, setThemeClasses] = useState('bg-gray-700 h-full');
+  const [themeClasses, setThemeClasses] = useState('bg-stone-950 h-full');
 
   useEffect(() => {
     setThemeClasses(
       theme.color === 'light'
-        ? 'bg-gray-400 h-full' 
-        : 'bg-stone-950 h-full'
+        ? 'bg-gray-600 h-full text-2xl font-bold text-stone-950'
+        : 'bg-stone-950 h-full text-2xl font-bold text-slate-200'
     );
   }, [theme.color]);
 
   return (
     <div className={themeClasses}>
-      <div className="flex justify-center items-center h-full">
+      <div className="flex flex-col justify-center items-center h-full">
         <Image 
           src='/myphoto.jpeg' 
           width={300} 
           height={200} 
           alt='Minha Foto'
-          className="rounded-full shadow-lg h-95 w-95 object-cover mx-auto my-8"
+          className="rounded-full shadow-lg h-95 w-95 object-cover my-4"
         />
+        <div className="text-center">
+          <h2 className={themeClasses}>Desenvolvedor Front-end</h2>
+        </div>
       </div>
     </div>
   );
 }
-
 
 export default Nav;
 
